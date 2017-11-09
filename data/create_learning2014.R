0cd6efe69d515f879f7676590d73b547e7e7ad9e
roopekaaronen <33350751+roopekaaronen@users.noreply.github.com>
2017-11-09 10:29:26 +0200

    Exercise 2
    10:29 9.11.

---
+++ data/create_learning2014.R
@@ -0,0 +1,81 @@
+#Roope Kaaronen, 9.11., Data Wrangling Exercise
+
+install.packages("dplyr")
+
+library(dplyr)
+
+lrn14 <- read.table("http://www.helsinki.fi/~kvehkala/JYTmooc/JYTOPKYS3-data.txt", sep="\t", header=TRUE)
+
+str(lrn14)
+dim(lrn14)
+
+#The data has 183 rows and 60 columns (variables)
+
+#Create an analysis dataset with the variables gender, age, attitude, deep, stra, surf and points
+
+# questions related to deep, surface and strategic learning
+
+deep_questions <- c("D03", "D11", "D19", "D27", "D07", "D14", "D22", "D30", "D06",  "D15", "D23", "D31")
+
+surface_questions <- c("SU02","SU10","SU18","SU26", "SU05","SU13","SU21","SU29","SU08","SU16","SU24","SU32")
+
+strategic_questions <- c("ST01","ST09","ST17","ST25","ST04","ST12","ST20","ST28")
+
+# select the columns related to deep learning and create column 'deep' by averaging
+
+deep_columns <- select(lrn14, one_of(deep_questions))
+
+lrn14$deep <- rowMeans(deep_columns)
+
+# select the columns related to surface learning and create column 'surf' by averaging
+
+surface_columns <- select(lrn14, one_of(surface_questions))
+
+lrn14$surf <- rowMeans(surface_columns)
+
+# select the columns related to strategic learning and create column 'stra' by averaging
+
+strategic_columns <- select(lrn14, one_of(strategic_questions))
+
+lrn14$stra <- rowMeans(strategic_columns)
+
+# choose a handful of columns to keep
+
+keep_columns <- c("gender", "Age", "Attitude", "deep", "stra", "surf", "Points")
+
+# select the 'keep_columns' to create a new dataset
+
+learning2014 <- select(lrn14, one_of(keep_columns))
+
+# see the structure of the new dataset
+
+str(learning2014)
+
+# print out the column names of the data
+colnames(learning2014)
+
+# change the name of the second column
+colnames(learning2014)[2] <- "age"
+
+# change the name of "Points" to "points"
+colnames(learning2014)[7] <- "points"
+
+colnames(learning2014)[3] <- "attitude"
+
+colnames(learning2014)
+
+# select rows where points is greater than zero
+
+learning2014 <- filter(learning2014, points > 0)
+
+dim(learning2014)
+
+write.csv(learning2014, file = "learning2014.csv")
+
+#Reading learning2014.csv
+
+read.csv(file = "learning2014.csv")
+
+head(learning2014)
+str(learning2014)
+
