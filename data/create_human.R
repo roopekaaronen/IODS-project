ac1a391e2d5d00ea6e92d8329a75c4c52f0b1d38
Roope <roopekaaronen(At)gmail.com>
2018-11-27 22:11:18 +0200

    chapter5
    beginnings

--- data/create_human.R
+++ data/create_human.R
@@ -30,7 +30,46 @@ str(human)
 # Observations: 195
 # Variables: 19
 
-# Finally, write the csv
-write.csv(human, file = "human.csv")
+
 
 # All is set. DATA WRANGLING OVER ~~
+
+## CONTINUED: CHAPTER 5
+# Explore structure and dimensions
+dim(human)
+str(human)
+
+# The data includes 195 observations of 19 variables
+# The variables are renamed in the code above. M.Ed = Female.Ed / Male.Ed & FM.Lab = Labour.F / Labour.M
+
+# mutate
+library(stringr)
+human$GNI <- str_replace(human$GNI, pattern=",", replace ="") %>% as.numeric
+
+keep <- c("Country", "FM.Ed", "FM.Lab", "Life.Exp.", "Exp.Ed", "GNI", "Maternal.Mortality", "Adol.Birth", "Parliament")
+
+human <- dplyr::select(human, one_of(keep))
+
+# Remove all rows with missing values
+human <- filter(human, complete.cases(human))
+
+# last indice we want to keep
+last <- nrow(human) - 7
+
+# Remove the observations which relate to regions instead of countries
+
+human <- human[1:last, ]
+
+# add countries as rownames
+rownames(human) <- human$Country
+
+dim(human)
+str(human)
+View(human)
+
+# 155 observations from 9 variables. Looks good.
+
+# Finally, write the csv
+write.csv(human, file = "human.csv", row.names = TRUE) 
+
+?write.csv
